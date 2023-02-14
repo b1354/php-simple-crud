f3497440fe2495adf63825cfb6c12f48228b6ccd
b1354 <bayurizkyseptiansyah@gmail.com>
2023-02-14 10:10:15 +0700

    fixing hapus.php

--- src/hapus.php
+++ src/hapus.php
@@ -4,6 +4,10 @@
   checkCookie($_COOKIE);
   checkSession("user_data", "login.php");
   
+  if (!isset($_GET["id"])) {
+    header("Location: index.php");
+  }
+    
   $id = $_GET["id"];
 
   if ( hapus($id) > 0 ) {
